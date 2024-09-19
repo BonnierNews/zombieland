a4b00e296c254eeee56fc0894ee2d83b9ba0c8f5
Jonas Waldén <jonas.walden@expressen.se>
2024-09-19 22:42:17 +0200

    tests passing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export * from 'jsdom';
-export * from './lib/tallahassee';
-export * from './lib/little-rock';
-export * from './lib/wichita';
+export * from './lib/tallahassee/index.js';
+export * from './lib/little-rock/index.js';
+export * from './lib/wichita/index.js';
