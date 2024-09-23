a667cd60691665a69d331c2c1205a00cd57d0cd2
Jonas Waldén <jonas.walden@expressen.se>
2024-09-23 21:21:27 +0200

    better test script

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -3,7 +3,6 @@
 module.exports = {
 	'recursive': true,
 	'extension': [ 'js' ],
-	'spec': [ '**/*-test.js', 'examples/*/test.js' ],
-	'ui': 'mocha-cakes-2',
 	'node-option': [ 'experimental-vm-modules', 'no-warnings' ],
+	'ui': 'mocha-cakes-2',
 };
