e9bd976be9db9e6581a60c04a0c098c054c28246
Jonas Waldén <jonas.walden@expressen.se>
2024-10-03 00:30:57 +0200

    rename some stuff, edit exports,

--- examples/polyfill/test.js
+++ examples/polyfill/test.js
@@ -1,7 +1,7 @@
 import assert from 'node:assert/strict';
 import server from './server.js';
 import setup from '../helpers/setup.js';
-import { Browser, Resources } from '../../index.js';
+import { Browser, ResourceLoader } from '../../index.js';
 
 Feature('polyfill', () => {
 	const pendingServerOrigin = setup(server);
@@ -14,7 +14,7 @@ Feature('polyfill', () => {
 				runScripts: 'dangerously',
 				beforeParse: window => {
 					assert.equal('fetch' in window, false);
-					Resources.polyfill(window);
+					ResourceLoader.polyfill(window);
 				},
 			});
 	});
