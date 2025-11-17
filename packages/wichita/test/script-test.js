59510fd8fd040a78a873276d5e7d83ca2206ba70
Jonas Waldén <jonas.walden@expressen.se>
2025-11-17 12:04:35 +0100

    fix/refactor script tests in wichita

--- packages/wichita/test/script-test.js
+++ packages/wichita/test/script-test.js
@@ -1,63 +1,77 @@
 import assert from 'node:assert/strict';
 import Script from '../script.js';
+import { JSDOM } from 'jsdom';
 
 describe('Script', () => {
 	describe('.evaluate()', () => {
-		it('evaluates code in a given context', async () => {
+		it('evaluates script from file path', async () => {
+			const module = new Script(import.meta.dirname + '/files/source-entry.mjs');
+			const dom = new JSDOM('<title>initial value</title>');
+
+			await module.evaluate(dom.window);
+
+			assert.equal(dom.window.document.title, 'initial value, edit from source entry, edit from source component');
+		});
+
+		it('evaluates script from code', async () => {
 			const module = new Script(`
 				document.title += ', edit from script';
 			`);
-			const document = { title: 'initial value' };
-			await module.evaluate({ document });
+			const dom = new JSDOM('<title>initial value</title>');
 
-			assert.equal(document.title, 'initial value, edit from script');
+			await module.evaluate(dom.window);
+
+			assert.equal(dom.window.document.title, 'initial value, edit from script');
 		});
 
-		it('evaluates code with imports', async () => {
-			const module = new Script(import.meta.filename, `
+		it('evaluates script with imports', async function () {
+			const module = new Script(getTestPath(this), `
 				import component from './files/source-component.mjs';
-				import * as npmModule from 'assertion-error';
-				if (npmModule)
-					document.title += ', edit based on npm module';
+				import packageComponent from 'package-component';
 				component();
+				packageComponent();
 			`);
-			const document = { title: 'initial value' };
-			await module.evaluate({ document });
-			assert.equal(document.title, 'initial value, edit based on npm module, edit from source component');
+			const dom = new JSDOM('<title>initial value</title>');
+
+			await module.evaluate(dom.window);
+
+			assert.equal(dom.window.document.title, 'initial value, edit from source component, edit from package component');
 		});
 
-		it('evaluates code with exports', async () => {
-			const module = new Script(`
+		it('evaluates script with exports', async function () {
+			const module = new Script(getTestPath(this), `
 				export default document.title + '?';
 				export const named = document.title + '!';
 			`);
-			const exports = await module.evaluate({
-				document: { title: 'with exports' },
-			});
+			const dom = new JSDOM('<title>with exports</title>');
+
+			const exports = await module.evaluate(dom.window);
+
 			assert.equal(exports.default, 'with exports?');
 			assert.equal(exports.named, 'with exports!');
 		});
 
-		it('evaluates code from path', async () => {
-			const module = new Script(import.meta.dirname + '/files/source-entry.mjs');
-			const document = { title: 'initial value' };
-			await module.evaluate({ document });
-			assert.equal(document.title, 'initial value, edit from source entry, edit from source component');
-		});
-
-		it('evaluates code multiple times', async () => {
-			const module = new Script(`
+		it('evaluates script multiple times', async function () {
+			const module = new Script(getTestPath(this), `
 				let i = 0;
 				export default document.title + '!';
 				export const times = ++i;
 			`);
+
 			const exports = await Promise.all([
-				module.evaluate({ document: { title: 'once' } }),
-				module.evaluate({ document: { title: 'twice' } }),
+				module.evaluate(new JSDOM('<title>once</title>').window),
+				module.evaluate(new JSDOM('<title>twice</title>').window),
 			]);
+
 			assert.equal(exports[0].default, 'once!');
 			assert.equal(exports[1].default, 'twice!');
 			assert.equal(exports[1].times, 1);
 		});
 	});
 });
+
+function getTestPath (context) {
+	return import.meta.filename + '?' + new URLSearchParams({
+		test: context.test.title
+	});
+}
